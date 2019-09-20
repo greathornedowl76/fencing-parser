96f5c020c32464cf21c568d313ce1069a078f538
ravenseattuna <46833768+ravenseattuna@users.noreply.github.com>
2019-09-20 02:42:36 -0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\st123\AppData\Local\Tesseract-OCR\tesseract.exe'
-
-
-print(pytesseract.image_to_string("Screen Shot 2018-09-02 at 6.48.10 PM.png"))
